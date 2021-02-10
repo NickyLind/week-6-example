9ec1bbb980a0e2c95b1852613373d00aa8afdf77
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-09 17:59:57 -0800

    fix title

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Weather Tracker',
       template: './src/index.html',
       inject: 'body'
     })
