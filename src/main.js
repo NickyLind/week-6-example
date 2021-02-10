d9b40da355a2ad46a6621fb426cd4c89dd92d53b
Nick Lindau <nicholaithegreat@gmail.com>
2021-02-09 17:58:03 -0800

    add async functionality

--- src/main.js
+++ src/main.js
@@ -16,17 +16,19 @@ function getElements(response)  {
     $(".showHumidity").text(`the humidity in ${city} is ${body.main.humidity}%`);
     $(".showTemp").text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
   } else  {
-    $(".showErrors").text(`There was an error: ${response.message}`)
+    $(".showErrors").text(`There was an error: ${response}`)
   }
+}
+
+async function makeApiCall(city)  {
+  const response = await WeatherService.getWeather(city);
+  getElements(response)
 }
 
 $(document).ready(function()  {
   $('#weatherLocation').click(function()  {
     let city = $('#location').val();
     clearFields();
-    WeatherService.getWeather(city)
-      .then(function(response)  {
-        getElements(response)
-      });
+    makeApiCall(city);
   });
 });
